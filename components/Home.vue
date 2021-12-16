9b93de3300adf90c7325204fd185412772cb5bec
maheshwohlig <maheshwohlig@gmail.com>
2021-12-16 09:46:34 +0530

    created

---
+++ components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-col cols="12">
+      <v-img
+        src="https://developers.google.com/codelabs/maps-platform/maps-platform-101-js/img/e52623cb8578d625.png"
+        height="100vh"
+      >
+        <v-toolbar class="new d-flex justify-center rounded-pill" dark>
+          <v-text-field hide-details prepend-icon="mdi-magnify"></v-text-field>
+        </v-toolbar>
+      </v-img>
+    </v-col>
+  </div>
+</template>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+.new {
+  width: 50%;
+  margin-left: 25%;
+  border-radius: 10px;
+}
+</style>
